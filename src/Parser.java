ac529692202b72515f57cba439af580c56967b2c
basel68 <baselfarouk09@gmail.com>
2023-05-13 13:10:03 +0300

    file changes

--- src/Parser.java
+++ src/Parser.java
@@ -1,51 +1,117 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Parser {
-     static Memory memory=Memory.getInstance();
+     static String[] memory= Memory.getInstance().getMemory();
      Queue<Integer> Ready = new LinkedList<Integer>();
      Queue<Integer> GeneralBlocked = new LinkedList<Integer>();
      Queue<Integer> InputBlocked = new LinkedList<Integer>();
      Queue<Integer> OutputBlocked = new LinkedList<Integer>();
      Queue<Integer> fileBlocked = new LinkedList<Integer>();
-     File HardDisk = new File("Disk");
+     static String[] Disk=new String[20];
 
-     public static void changeState(PCB pcb,String state) {
-          pcb.setState(state);
-
-
-
-     }
-     public static int ifSpace(String[] mem) {
-          if (mem[0].equals("")) {
+     public static int spaceAvailable(String[] mem) {
+          if (mem[0]==null|| mem[0].equals("")) {
                return 0;
-          } else if (mem[15]==null || mem[15].equals("")) {
-               return 15;
+          } else if (mem[20]==null || mem[20].equals("")) {
+               return 20;
           } else
                return -1;
      }
-     public static void createProcess(String path) throws FileNotFoundException {
-     int pcbId=PCB.idReached++;
-     String state= "new";
-     int pc=0;
-     int memStart=ifSpace(memory.getMemory());
-     int memEnd=ifSpace(memory.getMemory())==0?15:30;
+     public static int SwapMemToDisk(){
+          int emptied=-1;
+          if (!(memory[1].equals("Running"))) {
+               System.out.println("process with id"+ memory[0]+ "is swapped to disk");
+               for (int i = 0; i < 20; i++) {
+                    Disk[i] = memory[i];
+                    memory[i] = "";
+                    emptied= 0;
+               }
 
+            }
+          else{
+               System.out.println("process with id"+ memory[20]+ "is swapped to disk");
+               for (int i = 0; i < 20; i++) {
+                    Disk[i] = memory[i];
+                    memory[i] = "";
+                    emptied= 20;
+               }
+          }
+          return emptied;
+     }
+     public static void createProcess(String path) throws IOException {
+          int pcbId=PCB.idReached++;
+          String state= "new";
+          int pc=-1;
+          int memStart=-1;
+          int memEnd=-1;
+          if (spaceAvailable(memory)==-1){
 
+               //if memory is full
 
+          } else {
+               if(spaceAvailable(memory)==0){
+                   pc=8;
+               }else{
+                    pc=28;
+               }
 
+               memStart= spaceAvailable(memory);
+               memEnd = spaceAvailable(memory) == 0 ? 19 : 39;
 
+          }
+
+          PCB pcb = new PCB(pcbId,state,pc,memStart,memEnd);
+          saveInMemory(pcb,path);
 
 
      }
-     public static void saveToMemory( PCB pcb,String path) throws FileNotFoundException {
+     public static void saveInMemory(PCB pcb, String path) throws IOException {
           File file = new File(path);
           BufferedReader br = new BufferedReader(new FileReader(file));
 
+     }
+
+     public  void changeState(PCB pcb,String state) {
+          pcb.setState(state);
+          if (pcb.getpId() == 1) {
+               if (memory[0].equals("1")) {
+                    memory[1] = state;
+               } else if (memory[20].equals("1")) {
+                    memory[21] = state;
+               }
+
+          }
+          else if (pcb.getpId() == 2) {
+
+               if (memory[0].equals("2")) {
+                    memory[1] = state;
+               } else if (memory[20].equals("2")) {
+                    memory[21] = state;
+               }
+          }
+          else if (pcb.getpId() == 3) {
+
+               if (memory[0].equals("3")) {
+                    memory[1] = state;
+               } else if (memory[20].equals("3")) {
+                    memory[21] = state;
+               }
+          }
+
+     }
+     public static void emptyMemory(PCB pcb) {
+          if(memory[0].equals(pcb.getpId()+"")) {
+               for(int i = 0; i < 20; i++) {
+                    memory[i]="";
+               }
+          }
+          else if(memory[20].equals(pcb.getpId()+"")) {
+               for(int i = 20; i < 40; i++) {
+                    memory[i]="";
+               }
+          }
      }
 
 
