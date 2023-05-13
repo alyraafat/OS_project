846219a5feef695e7b6b53aaf8d5a6fd22c22805
basel68 <baselfarouk09@gmail.com>
2023-05-14 00:37:41 +0300

    changes

--- src/Parser.java
+++ src/Parser.java
@@ -4,16 +4,23 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Parser {
+     static String input1;
+     static String input2;
+     static String input3;
+     String readFile;
      static String[] memory= Memory.getInstance().getMemory();
      Queue<Integer> Ready = new LinkedList<Integer>();
-     Queue<Integer> generalBlocked = new LinkedList<Integer>();
+     static Queue<Integer> generalBlocked = new LinkedList<Integer>();
      Queue<Integer> inputBlocked = new LinkedList<Integer>();
      Queue<Integer> outputBlocked = new LinkedList<Integer>();
      Queue<Integer> fileBlocked = new LinkedList<Integer>();
+     static Mutex userInput = new Mutex("userInput");
+     static Mutex userOutput = new Mutex("userOutput");
+     static Mutex file = new Mutex("file");
      static String[] Disk=new String[20];
      static  String[] temp=new String[20];
      static SystemCall systemCall=new SystemCall();
-     public void updatePC(PCB pcb) {
+     public static void updatePC(PCB pcb) {
           int x= pcb.getPc();
           if (pcb.getpId() == 1) {
 
@@ -133,9 +140,9 @@ public class Parser {
           return pcb;
 
      }
-     public static int execute(PCB pcb,int timeSlice) throws IOException {
+     public int execute(PCB pcb, int timeSlice) throws IOException {
           int pcValue = pcb.getPc();
-
+          String Input="";
           for (int i = pcValue; i < pcValue + timeSlice && i < pcb.getMemEnd(); i++) {
                if (memory[i].equals("null")) {
                     pcb.setPc(pcb.getMemEnd());
@@ -144,71 +151,90 @@ public class Parser {
                String[] y = memory[i].split(" ");
                System.out.println("The Instruction that's currently executing is " + memory[i] + " in Process " + pcb.getpId());
                System.out.println("********");
-//               if (y[0].equals("print")) {
-//                    systemCall.print(y[1], pcb);
-//               }
-//               // Storing variables
-//               else if (y[0].equals("input")) {
-//                    System.out.println("Please enter an input: ");
-//                    Scanner sc = new Scanner(System.in);
-//                    x = sc.nextLine();
-//                    if (pcb.getpID() == 1) {
-//                         input1 = x;
-//                    } else if (pcb.getpID() == 2) {
-//                         input2 = x;
-//                    } else if (pcb.getpID() == 3) {
-//                         input3 = x;
-//                    }
-//               } else if (y[0].equals("readFile")) {
-//                    systemCall.readFile(readMemory(pcb, y[1]));
-//
-//
-//               } else if (y[0].equals("assign")) {
-//                    if (y[2].equals("input")) {
-//                         if (pcb.getpID() == 1) {
-//                              systemCall.assign(y[1], input1, pcb);
-//                         } else if (pcb.getpID() == 2) {
-//                              systemCall.assign(y[1], input2, pcb);
-//                         } else if (pcb.getpID() == 3) {
-//                              systemCall.assign(y[1], input3, pcb);
-//                         }
-//                    } else if (y[2].equals("readFile")) {
-//                         systemCall.assign(y[1], file3, pcb);
-//                    }
-//               } else if (y[0].equals("writeFile")) {
-//
-//                    systemCall.writeFile(readMemory(pcb, y[1]), readMemory(pcb, y[2]));
-//
-//               } else if (y[0].equals("printFromTo")) {
-//                    systemCall.printFromTo(y[1], y[2], pcb);
-//               } else if (y[0].equals("semWait")) {
-//                    if (y[1].equals("userInput")) {
-//                         userInput.semWait(y[1], pcb);
-//                    } else if (y[1].equals("userOutput")) {
-//                         userOutput.semWait(y[1], pcb);
-//                    } else if (y[1].equals("file")) {
-//                         file.semWait(y[1], pcb);
-//                    }
-//
-//                    if (GeneralBlocked.contains(pcb.getpID())) {
-//                         changePC(pcb);
-//                         //changeState(pcb,"Blocked");
-//                         break;
-//                    }
-//               } else if (y[0].equals("semSignal")) {
-//                    if (y[1].equals("userInput")) {
-//                         userInput.semSignal(y[1], pcb);
-//                    } else if (y[1].equals("userOutput")) {
-//                         userOutput.semSignal(y[1], pcb);
-//                    } else if (y[1].equals("file")) {
-//                         file.semSignal(y[1], pcb);
-//                    }
-//               }
+               if (y[0].equals("print")) {
+                    systemCall.print( pcb,y[1]);
+               }
+               else if (y[0].equals("readFile")) {
+                    systemCall.readFile(readFromMemory(pcb, y[1]));
+
+
+               } else if (y[0].equals("assign")) {
+                    if (y[2].equals("input")) {
+                         System.out.println("Please enter an input: ");
+                         Scanner sc = new Scanner(System.in);
+                         Input = sc.nextLine();
+                         if (pcb.getpId() == 1) {
+                              input1 = Input;
+                         } else if (pcb.getpId() == 2) {
+                              input2 = Input;
+                         } else if (pcb.getpId() == 3) {
+                              input3 = Input;
+                         }
+                         if (pcb.getpId() == 1) {
+                              systemCall.assign(y[1], input1, pcb);
+                         } else if (pcb.getpId() == 2) {
+                              systemCall.assign(y[1], input2, pcb);
+                         } else if (pcb.getpId() == 3) {
+                              systemCall.assign(y[1], input3, pcb);
+                         }
+                    } else if (y[2].equals("readFile")) {
+                         systemCall.assign(y[1], file3, pcb);
+                    }
+               } else if (y[0].equals("writeFile")) {
+
+                    systemCall.writeFile(this.readFromMemory(pcb, y[1]), this.readFromMemory(pcb, y[2]));
+
+               } else if (y[0].equals("printFromTo")) {
+                    systemCall.printFromTo(y[1], y[2], pcb);
+               } else if (y[0].equals("semWait")) {
+                    if (y[1].equals("userInput")) {
+                         userInput.semWait(y[1], pcb);
+                    } else if (y[1].equals("userOutput")) {
+                         userOutput.semWait(y[1], pcb);
+                    } else if (y[1].equals("file")) {
+                         file.semWait(y[1], pcb);
+                    }
+
+                    if (generalBlocked.contains(pcb.getpId())) {
+                         updatePC(pcb);
+
+                         break;
+                    }
+               } else if (y[0].equals("semSignal")) {
+                    if (y[1].equals("userInput")) {
+                         userInput.semSignal(y[1], pcb);
+                    } else if (y[1].equals("userOutput")) {
+                         userOutput.semSignal(y[1], pcb);
+                    } else if (y[1].equals("file")) {
+                         file.semSignal(y[1], pcb);
+                    }
+               }
 
 
 
           }return 0;
      }
+
+     public String readFromMemory(PCB pcb, String needed) {
+          if (memory[0].equals(pcb.getpId()+"")) {
+               for (int i = 5; i < 8; i++) {
+                    String[] y = memory[i].split(" ");
+                    if (y[0].equals(needed)) {
+                         return y[1];
+                    }
+               }
+          } else if (memory[20].equals(pcb.getpId()+"")) {
+               for (int i = 25; i < 28; i++) {
+                    String[] y = memory[i].split(" ");
+                    if (y[0].equals( needed)) {
+                         return y[1];
+                    }
+               }
+
+          }
+          return needed;
+     }
+
      public static void saveInMemory(PCB pcb, String path) throws IOException {
           File file = new File(path);
           BufferedReader br = new BufferedReader(new FileReader(file));
--- src/SystemCall.java
+++ src/SystemCall.java
@@ -1,12 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SystemCall {
+    static Parser parser=new Parser();
     public SystemCall() {
 
     }
-    public void print(){
+    public void print(PCB pcb,String needed){
+        System.out.println(parser.readFromMemory(pcb,needed));
+    }
+    public static void assign(String x, String y, PCB pcb) {
 
     }
-    public static void assign(String x, String y, PCB pcb) {}
     public static void writeFile(String fileName, String data) {}
-    public static void readFile(String fileName)  {}
+    public static void readFile(String fileName) throws FileNotFoundException {
+        File needed = new File(fileName);
+        Scanner myReader = new Scanner(needed);
+        parser.readFile=myReader.next();
+
+    }
     public static void printFromTo(String a, String b, PCB pcb) {}
 }
